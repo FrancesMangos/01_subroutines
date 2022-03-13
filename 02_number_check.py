9c28caf5ceddd94a105bbae17a18252805953d0d
magno.franc <magno.franc@awatapu.school.nz>
2022-03-14 10:07:46 +1300

    put the conditionals BEFORE the break

--- 02_number_check.py
+++ 02_number_check.py
@@ -2,13 +2,11 @@ def int_check(question):
 	while True:
 		try:
 			response = int(input(question))
+			if response <= 5:
+				print("You are too young to to de doing this by yourself")
 			break
 		except ValueError:
 			print("Not a valid age")
 	return response
 
-
 age = int_check("Enter your age!")
-if age >= 5:
-    print("You are too young to to de doing this by yourself")
-
