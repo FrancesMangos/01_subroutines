59961049d684644cbc932e23726d7054c6325bc2
magno.franc <magno.franc@awatapu.school.nz>
2022-03-15 11:31:00 +1300

    jordan helped me do this

--- 02_string_check.py
+++ 02_string_check.py
@@ -2,27 +2,27 @@ def int_check(question):
 	while True:
 		try:
 			response = int(input(question))
-			if response <= 5:
-			    print("You are too young to to de doing this by yourself")
-            else:
-			    break
+			if response >= 5:
+			    return response
+
+			print("You are too young to to de doing this by yourself")
 		except ValueError:
 			print("Not a valid age")
-    return response
+
 
 def check_input(question):
     while True:
         try:
-            response = input(question)
-            if response.isdigit():
+            response = input(question).title()
+            if response.isdigit() or response == "":
                 print("That is not a valid name")
             else:
-                break
+                return response
         except ValueError:
             print("Not valid name")
-    return response
 
 age = int_check("Enter your age:")
 name = check_input("Enter your name:")
 
 print("So your name is {} and you are {} years old?".format(name, age))
+
